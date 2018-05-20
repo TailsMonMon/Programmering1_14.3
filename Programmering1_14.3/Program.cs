9f4e437393c4346c6a16f578cd642d6657064c13
Monica Björk <m.bjork95@gmail.com>
2018-05-20 11:15:57 +0200

    Add project files.

---
+++ Programmering1_14.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_14._3 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Skriv ett tal för att räkna ut dess Fibonacci: ");
+            Console.WriteLine(Fibonacci(Convert.ToInt32(Console.ReadLine())));
+            Console.ReadKey();
+        }
+
+        static int Fibonacci(int f) {
+            if(f <= 2)
+                return 1;
+            else
+                return Fibonacci(f - 1) + Fibonacci(f - 2);
+        }
+    }
+}
